447b63a19f7b87dd398de19875d37e87e22bc375
Jm Arreola <jmarreola391@gmail.com>
2024-03-28 11:44:39 -0600

    fix

--- Landing/src/app/page.js
+++ Landing/src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
                     <div className="container">
                         <div className="grid grid-cols-1">
                             <div className="md:text-start text-center">
-                                <h1 className="font-bold text-white lg:leading-normal leading-normal text-4xl lg:text-5xl mb-6">Te vamos a ayudar <br /> your <span className="text-green-600">Wonderful</span> home</h1>
+                                <h1 className="font-bold text-white lg:leading-normal leading-normal text-4xl lg:text-5xl mb-6">We'll help you find <br /> your <span className="text-green-600">Wonderful</span> home</h1>
                                 <p className="text-white/70 text-xl max-w-xl">A great plateform to buy, sell and rent your properties without any agent or commisions.</p>
                             </div>
                           </div>
